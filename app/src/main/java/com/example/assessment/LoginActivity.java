c02603db6df52b176b1ed2f1bcb2a4d9b57c262c
Ringku 13 <ringku@softbdltd.com>
2019-12-15 12:58:50 +0600

    Login Activity Done

--- app/src/main/java/com/example/assessment/LoginActivity.java
+++ app/src/main/java/com/example/assessment/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.assessment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.ViewCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,17 +11,19 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+CardView big_card;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        big_card=(CardView) findViewById(R.id.big_cart);
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         CardView cardView= (CardView)findViewById(R.id.big_cart);
-
         cardView.setBackgroundResource(R.drawable.big_card_bg_radius);
+        ViewCompat.setElevation(findViewById(R.id.imageView2),big_card.getElevation() * 2);
+
 
     }
 }
